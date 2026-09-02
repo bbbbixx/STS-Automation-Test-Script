b62923ad6551b28e2cf090b1543f48c2cf174e1b
Thanapat Chalaipoom <ThanapatChalaipoom@gmail.com>
2026-09-02 18:22:50 +0700

    refactor: remove unused files

--- tests/FN-STS-02/TC-STS-02-01-01.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Default Test Case', async ({ page }) => {
-  // TODO: Add test steps
-  await page.goto((process.env.TARGET_URL as string));
-
-});
